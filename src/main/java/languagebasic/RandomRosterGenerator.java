4df34329f3cf0bfbe7ffd6b6b80b4c761d724d3d
liem.chau <liem.chau@sandata.com>
2020-11-23 09:16:27 +0700

    implement getRandomePoster

--- src/main/java/languagebasic/RandomRosterGenerator.java
+++ src/main/java/languagebasic/RandomRosterGenerator.java
@@ -1,5 +1,8 @@
 package languagebasic;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Generate a random roster to review assignments =]]
  */
@@ -12,7 +15,45 @@ public class RandomRosterGenerator {
      * @return A roster with the desired size or empty if invalid input
      */
     public String[] getRandomRoster(String[] availableNames, int size) {
-        //TODO: implement this method
-        throw new UnsupportedOperationException("This method is not implemented yet");
+        ArrayList<Integer> list = getRandomNonRepeatingIntegers(size, 0, size);
+        try {
+            String[] listResult = new String[size];
+            for (int i = 0; i < size; i++) {
+                listResult[i] = availableNames[list.get(i)];
+            }
+            return listResult;
+        }catch (Exception ex){
+            return null;
+        }
+    }
+
+    /**
+     * get a random int form min to max
+     * @param min min int
+     * @param max max int
+     * @return the random int
+     */
+    public int getRandomInt(int min, int max) {
+        Random random = new Random();
+        return random.nextInt((max - min) + 1) + min;
+    }
+
+    /**
+     * Get the random int list from max to min with size of list
+     * @param size size list
+     * @param min min index
+     * @param max max index
+     * @return random array index not duplicate
+     */
+    public ArrayList<Integer> getRandomNonRepeatingIntegers(int size, int min,
+                                                                   int max) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        while (numbers.size() < size) {
+            int random = getRandomInt(min, max);
+            if (!numbers.contains(random)) {
+                numbers.add(random);
+            }
+        }
+        return numbers;
     }
 }
