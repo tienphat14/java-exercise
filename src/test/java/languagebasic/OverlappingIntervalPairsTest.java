afdb5e0f66b34d6f94dd381b95ddb8007c0755a8
liem.chau <liem.chau@sandata.com>
2020-11-28 15:26:07 +0700

    Rename the test case method

--- src/test/java/languagebasic/OverlappingIntervalPairsTest.java
+++ src/test/java/languagebasic/OverlappingIntervalPairsTest.java
@@ -20,7 +20,7 @@ public class OverlappingIntervalPairsTest {
     }
 
     @Test(dataProvider = NORMAL_ARRAY)
-    public void getArrayPacking_normalArray_returnTheIntegerNumberMore2Items(int[][] availableNumbers) {
+    public void getOverlappingIntervalPairs_normalArray_TheResultAsExpectedResult(int[][] availableNumbers) {
         int[][][] result  = overlappingIntervalPairs.getTheCoupleArrayOverlapRange(availableNumbers);
         int[][][] expectedResult = new int[][][]{{{11,15},{15,18}},{{11, 15},{13, 16}},{{3, 9},{1, 4}},{{15, 18},{13, 16}}};
         Assert.assertTrue(Arrays.deepEquals(expectedResult, result));
