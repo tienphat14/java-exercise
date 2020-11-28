3522747122983e1bc26b24bf7634982b732b1610
liem.chau <liem.chau@sandata.com>
2020-11-28 18:05:14 +0700

    Update the test script OverlappingIntervalPairsTest

--- src/test/java/languagebasic/OverlappingIntervalPairsTest.java
+++ src/test/java/languagebasic/OverlappingIntervalPairsTest.java
@@ -9,20 +9,36 @@ import java.util.Arrays;
 public class OverlappingIntervalPairsTest {
 
     private static final String NORMAL_ARRAY = "normalArray";
+    private static final String NON_EXISTING_OVERLAP_ARRAY = "noneOverlapArray";
+    private static final String MULTI = "Multi";
 
     OverlappingIntervalPairs overlappingIntervalPairs = new OverlappingIntervalPairs();
+
+    @DataProvider(name = NON_EXISTING_OVERLAP_ARRAY)
+    public Object[][] nonOverlapArrayProvider() {
+        return new Object[][]{
+                {new int[][]{{12, 15},{3, 9},{10, 11},{17, 20}}},
+                {new int[][]{{}}}
+        };
+    }
 
     @DataProvider(name = NORMAL_ARRAY)
     public Object[][] normalArrayProvider() {
         return new Object[][]{
-                {new int[][]{{11, 15},{3, 9},{1, 4},{15, 18}, {13, 16}}}
+                {new int[][]{{11, 15},{3, 9},{1, 4},{15, 18}, {13, 16}, {1, 2}}}
         };
     }
 
     @Test(dataProvider = NORMAL_ARRAY)
     public void getOverlappingIntervalPairs_normalArray_TheResultAsExpectedResult(int[][] availableNumbers) {
         int[][][] result  = overlappingIntervalPairs.getTheCoupleArrayOverlapRange(availableNumbers);
-        int[][][] expectedResult = new int[][][]{{{11,15},{15,18}},{{11, 15},{13, 16}},{{3, 9},{1, 4}},{{15, 18},{13, 16}}};
+        int[][][] expectedResult = new int[][][]{{{11,15},{15,18}},{{11, 15},{13, 16}},{{3, 9},{1, 4}},{{1, 4}, {1, 2}},{{15, 18},{13, 16}}};
         Assert.assertTrue(Arrays.deepEquals(expectedResult, result));
+    }
+
+    @Test(dataProvider = NON_EXISTING_OVERLAP_ARRAY)
+    public void getOverlappingIntervalPairs_nonOverlapArray(int[][] availableNumbers) {
+        int[][][] result  = overlappingIntervalPairs.getTheCoupleArrayOverlapRange(availableNumbers);
+        Assert.assertEquals( result.length, 0);
     }
 }
