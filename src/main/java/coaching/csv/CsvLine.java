db030370cf25ffbddcd81ffc98691c72e3de089b
liem.chau <liem.chau@sandata.com>
2020-12-06 18:42:21 +0700

    Implement CsvLine

--- src/main/java/coaching/csv/CsvLine.java
+++ src/main/java/coaching/csv/CsvLine.java
@@ -1,33 +1,126 @@
 package coaching.csv;
 
-import java.util.List;
+import java.io.Serializable;
+import java.util.*;
 
 /**
  * Supplies information about CSV line
  */
-public class CsvLine {
+public class CsvLine implements Serializable, Iterable<String>{
 
-    private int position;
-    private String data;
-    private List<String> listData;
-    /**
-     * Get data at specific position in a row
-     *
-     * @param index Position of segment
-     * @return Data
-     */
-    public String get(int index) throws  IndexOutOfBoundsException{
-        return listData.get(index);
+    private static final String[] EMPTY_STRING_ARRAY = new String[0];
+    private static final long serialVersionUID = 1L;
+    private long characterPosition;
+    private String comment;
+    private Map<String, Integer> mapping;
+    private long recordNumber;
+    private String[] values;
+
+    CsvLine(){}
+    
+    CsvLine(String[] values, Map<String, Integer> mapping, String comment, long recordNumber, long characterPosition) {
+        this.recordNumber = recordNumber;
+        this.values = values != null ? values : EMPTY_STRING_ARRAY;
+        this.mapping = mapping;
+        this.comment = comment;
+        this.characterPosition = characterPosition;
     }
 
-    /**
-     * Set data in a specific position
-     *
-     * @param position Position
-     * @param data     Data
-     */
-    public void set(int position, String data) {
-        this.position = position;
-        this.data = data;
+    public String get(Enum<?> e) {
+        return this.get(e.toString());
+    }
+
+    public String get(int i) {
+        return this.values[i];
+    }
+
+    public String get(String name) {
+        if (this.mapping == null) {
+            throw new IllegalStateException("No header mapping was specified, the record values can't be accessed by name");
+        } else {
+            Integer index = (Integer)this.mapping.get(name);
+            if (index == null) {
+                throw new IllegalArgumentException(String.format("Mapping for %s not found, expected one of %s", name, this.mapping.keySet()));
+            } else {
+                try {
+                    return this.values[index];
+                } catch (ArrayIndexOutOfBoundsException var4) {
+                    throw new IllegalArgumentException(String.format("Index for header '%s' is %d but CSVRecord only has %d values!", name, index, this.values.length));
+                }
+            }
+        }
+    }
+
+    public long getCharacterPosition() {
+        return this.characterPosition;
+    }
+
+    public String getComment() {
+        return this.comment;
+    }
+
+    public long getRecordNumber() {
+        return this.recordNumber;
+    }
+
+    public boolean isConsistent() {
+        return this.mapping == null || this.mapping.size() == this.values.length;
+    }
+
+    public boolean hasComment() {
+        return this.comment != null;
+    }
+
+    public boolean isMapped(String name) {
+        return this.mapping != null && this.mapping.containsKey(name);
+    }
+
+    public boolean isSet(String name) {
+        return this.isMapped(name) && (Integer)this.mapping.get(name) < this.values.length;
+    }
+
+    public Iterator<String> iterator() {
+        return this.toList().iterator();
+    }
+
+    <M extends Map<String, String>> M putIn(M map) {
+        if (this.mapping == null) {
+            return map;
+        } else {
+            Iterator var2 = this.mapping.entrySet().iterator();
+
+            while(var2.hasNext()) {
+                Map.Entry<String, Integer> entry = (Map.Entry)var2.next();
+                int col = (Integer)entry.getValue();
+                if (col < this.values.length) {
+                    map.put(entry.getKey(), this.values[col]);
+                }
+            }
+
+            return map;
+        }
+    }
+
+    public int size() {
+        return this.values.length;
+    }
+
+    private List<String> toList() {
+        return Arrays.asList(this.values);
+    }
+
+    public Map<String, String> toMap() {
+        return this.putIn(new HashMap(this.values.length));
+    }
+
+    public String toString() {
+        return "CSVRecord [comment=" + this.comment + ", mapping=" + this.mapping + ", recordNumber=" + this.recordNumber + ", values=" + Arrays.toString(this.values) + "]";
+    }
+
+    String[] values() {
+        return this.values;
+    }
+
+    public void set(int i, String firstName) {
     }
 }
