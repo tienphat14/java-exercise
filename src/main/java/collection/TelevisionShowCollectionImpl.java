7cc7e0e0ee810b94d452479ae35ce8a66b6bcafe
tuannnguyen <Tuan.Nguyen@sandata.com>
2020-12-15 15:03:20 +0700

    collection - IterableExcercise answer

--- src/main/java/collection/TelevisionShowCollectionImpl.java
+++ src/main/java/collection/TelevisionShowCollectionImpl.java
@@ -1,49 +1,92 @@
 package collection;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 public class TelevisionShowCollectionImpl implements TelevisionShowCollection {
+
+    private List<TelevisionShow> showList;
+
+    public TelevisionShowCollectionImpl() {
+        showList = new ArrayList<>();
+    }
 
     @Override
     public TelevisionShowIterator iterator() {
-        // TODO
-        throw new UnsupportedOperationException();
+        return new TelevisionShowIteratorImpl(showList);
     }
 
     @Override
     public void addShow(TelevisionShow show) {
-        // TODO
-        throw new UnsupportedOperationException();
+        showList.add(show);
     }
 
     @Override
     public TelevisionShowIterator channelIterator(Channel channel) {
-        // TODO
-        throw new UnsupportedOperationException();
+        return new TelevisionShowIteratorImpl(showList, channel);
     }
 
     private class TelevisionShowIteratorImpl implements TelevisionShowIterator {
 
+        private List<TelevisionShow> showList;
+        private Channel channel;
+        private int position = - 1;
+
+        private TelevisionShowIteratorImpl(List<TelevisionShow> showList) {
+            this.showList = showList;
+            this.channel = Channel.ALL;
+        }
+
+        private TelevisionShowIteratorImpl(List<TelevisionShow> showList, Channel channel) {
+            this.showList = showList;
+            this.channel = channel;
+        }
+
         @Override
         public boolean hasNext() {
-            // TODO
-            throw new UnsupportedOperationException();
+            if (position < -1) {
+                position = -1;
+            }
+            while (position < showList.size() - 1) {
+                TelevisionShow show = showList.get(position + 1);
+                if (channel.equals(Channel.ALL) || show.getChannel().equals(channel)) {
+                    return true;
+                }
+                position++;
+            }
+            return false;
         }
 
         @Override
         public TelevisionShow next() {
-            // TODO
-            throw new UnsupportedOperationException();
+            if (hasNext()) {
+                return showList.get(position++ + 1);
+            }
+            throw new NoSuchElementException();
         }
 
         @Override
         public boolean hasPrevious() {
-            // TODO
-            throw new UnsupportedOperationException();
+            if (position > showList.size() - 1) {
+                position = showList.size() - 1;
+            }
+            while (position > -1) {
+                TelevisionShow show = showList.get(position);
+                if (channel.equals(Channel.ALL) || show.getChannel().equals(channel)) {
+                    return true;
+                }
+                position--;
+            }
+            return false;
         }
 
         @Override
         public TelevisionShow previous() {
-            // TODO
-            throw new UnsupportedOperationException();
+            if (hasPrevious()) {
+                return showList.get(position--);
+            }
+            throw new NoSuchElementException();
         }
     }
 }
