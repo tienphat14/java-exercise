f4569751a9f65c133f66afe995729cdd294f9061
liem.chau <liem.chau@sandata.com>
2021-01-12 16:58:40 +0700

    Implement HashSetPractice 5 excersise

--- src/main/java/collection/HashSetPractice.java
+++ src/main/java/collection/HashSetPractice.java
@@ -3,7 +3,9 @@ package collection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class HashSetPractice {
@@ -12,18 +14,29 @@ public class HashSetPractice {
 
     public static void main(String[] args) {
         Set<String> hashSet = new HashSet<>();
-        // TODO: append some elements to the end of the set
+        // append some elements to the end of the set
+        hashSet.add("One");
+        hashSet.add("Two");
+        hashSet.add("Three");
+        hashSet.add("Four");
+        hashSet.add("Five");
+        LOGGER.info("{}",hashSet);
 
-        // TODO: get an iterate and use it to print out all elements in the set
-
-        // TODO: print out the number of elements in the set
-
-        // TODO: convert the set to a string array
-
-        // TODO: convert the set to a List/ArrayList
+        // get an iterate and use it to print out all elements in the set
+        hashSet.iterator().forEachRemaining(LOGGER::info);
+        // print out the number of elements in the set
+        LOGGER.info("size = {}", hashSet.size());
+        // convert the set to a string array
+        String[] arrayRes = hashSet.toArray(new String[0]);
+        for(String s: arrayRes){
+            LOGGER.info("{}", s);
+        }
+        // convert the set to a List/ArrayList
+        List<String> list = new ArrayList<>(hashSet);
 
         Set<String> newHashSet = new HashSet<>(hashSet);
-        // TODO: compare if two sets are equal
+        // compare if two sets are equal
+        newHashSet.equals(hashSet);
 
         newHashSet.add("Seven");
         newHashSet.add("Eight");
