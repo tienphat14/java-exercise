718a4e9118836ee18ea119ef03ac8d73026b81ec
nhont.nguyen <nnguyen@sandata.com>
2021-01-18 23:57:36 +0700

    do StreamAPI exercise 1,3,4

--- src/main/java/streamapi/StreamAPIExerciseSimple.java
+++ src/main/java/streamapi/StreamAPIExerciseSimple.java
@@ -3,9 +3,8 @@ package streamapi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static java.util.Arrays.asList;
@@ -16,17 +15,16 @@ public class StreamAPIExerciseSimple {
 
     public static void main(String[] args) {
         StreamAPIExerciseSimple excercise = new StreamAPIExerciseSimple();
-        LOGGER.info("{}", excercise.exercise1());
-        LOGGER.info("{}", excercise.exercise2());
+        LOGGER.info("Exercise 1: {}", excercise.exercise1());
+//        LOGGER.info("{}", excercise.exercise2());
         LOGGER.info("{}", excercise.exercise3());
         LOGGER.info("{}", excercise.exercise4());
     }
 
     public Set<Integer> exercise1() {
-        List<Integer> numbers = asList(1, 2, 3, 4, 5);
-        // TODO: divide each number by 2 (ignoring the reminder) and collect to set
+        List<Integer> numbers = asList(6, 7, 5, 2, 8);
         // [0, 1, 2]
-        throw new UnsupportedOperationException();
+        return numbers.stream().map(n -> n / 2).collect(Collectors.toSet());
     }
 
     public List<Integer> exercise2() {
@@ -40,13 +38,15 @@ public class StreamAPIExerciseSimple {
         List<String> strings = asList("Lorem", "ipsum", "dolor", "sit", "amet,", "consectetur", "adipiscing", "elit.");
         // TODO: find the length of the longest string
         // 11
-        throw new UnsupportedOperationException();
+        return strings.stream().max(Comparator.comparingInt(String::length)).get().length();
     }
 
     public List<Integer> exercise4() {
         IntStream numbers = new Random(0).ints().map(Math::abs);
         // TODO: find first 5 numbers divisible by 17 generate by an infinite stream
         // [938301587, 100082026, 356750287, 798819494, 1412716779]
-        throw new UnsupportedOperationException();
+        List<Integer> list = new ArrayList<>();
+        numbers.filter(n -> numbers.anyMatch(d -> d % 17 == 0)).limit(5).forEach(list::add);
+        return list;
     }
 }
