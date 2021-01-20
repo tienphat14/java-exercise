0089e91bde86fd1cf713506290175fdeae23286e
Triet Nguyen <triethnguyen@kms-technology.com>
2021-01-20 11:07:22 +0700

    Stream API complex exercise practice

--- src/main/java/streamapi/StreamAPIExerciseComplex.java
+++ src/main/java/streamapi/StreamAPIExerciseComplex.java
@@ -3,8 +3,11 @@ package streamapi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class StreamAPIExerciseComplex {
 
@@ -19,20 +22,38 @@ public class StreamAPIExerciseComplex {
     }
 
     public String exercise1() {
-        // TODO: find whether there are two employees with the same first name and surname and return the name
-        throw new UnsupportedOperationException();
+
+        return EMPLOYEES.stream()
+                .filter(e1 -> EMPLOYEES.stream()
+                        .anyMatch(e2 -> e1 != e2 && e1.getFirstName().equals(e2.getFirstName()) && e1.getSurname().equals(e2.getSurname())))
+                .map(e -> e.getFirstName() + " " + e.getSurname())
+                .findFirst()
+                .orElse("Not Found");
     }
 
     public long exercise2() {
-        // TODO: find the total number of groups of at least 5 employees living close to each other
-        // consider all employees with the same 2 first characters of the home address post code a single group
-        throw new UnsupportedOperationException();
+        // find the total number of groups of at least 5 employees living close to each other
+        // consider all employees with the same 2 first characters of the home address post code a single groupList<String> strings = List.of("a", "bb", "cc", "ddd");
+        return EMPLOYEES.stream()
+                        .map(e1-> e1.getHomeAddress())
+                        .map(p -> p.getPostCode().substring(0, 2))
+                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                        .values()
+                        .stream()
+                        .filter(l -> l >= 5)
+                        .count();
     }
 
     public List<String> exercise3() {
         DecimalFormat decimalFormat = new DecimalFormat("£#,###.00");
-        // TODO: find how much in total each company pays to their employees, order result by amount
+        // find how much in total each company pays to their employees, order result by amount
         // Barclays plc - £12,184,531.00
-        throw new UnsupportedOperationException();
+
+       return  EMPLOYEES.stream()
+                .collect(Collectors.groupingBy(employee -> employee.getCompany().getName(), Collectors.reducing(BigDecimal.ZERO, Employee::getSalary, BigDecimal::add)))
+                .entrySet()
+                .stream()
+                .map(x -> x.getKey() + " - " + decimalFormat.format(x.getValue()))
+                .collect(Collectors.toList());
     }
 }
