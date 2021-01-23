eec6d9869c2dc4442de922825c931f139673bdbc
hung.dang <hung.dang@sandata.com>
2021-01-23 13:42:24 +0700

    Stream API exercise

--- src/main/java/streamapi/StreamAPIExerciseComplex.java
+++ src/main/java/streamapi/StreamAPIExerciseComplex.java
@@ -3,8 +3,14 @@ package streamapi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.math.BigDecimal;
 import java.text.DecimalFormat;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class StreamAPIExerciseComplex {
 
@@ -19,20 +25,37 @@ public class StreamAPIExerciseComplex {
     }
 
     public String exercise1() {
-        // TODO: find whether there are two employees with the same first name and surname and return the name
-        throw new UnsupportedOperationException();
+        return EMPLOYEES.stream()
+                .filter(emp1 -> EMPLOYEES.stream()
+                        .anyMatch(emp2 -> emp1 != emp2
+                                && emp1.getFirstName().equals(emp2.getFirstName())
+                                && emp1.getSurname().equals(emp2.getSurname())
+                        )
+                )
+                .map(emp -> emp.getFirstName() + " " + emp.getSurname())
+                .findFirst()
+                .orElse("Not Found");
     }
 
     public long exercise2() {
-        // TODO: find the total number of groups of at least 5 employees living close to each other
-        // consider all employees with the same 2 first characters of the home address post code a single group
-        throw new UnsupportedOperationException();
+        return EMPLOYEES.stream()
+                .map(emp -> emp.getHomeAddress().getPostCode().substring(0, 2))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .values()
+                .stream()
+                .filter(n -> n >= 5)
+                .count();
     }
 
     public List<String> exercise3() {
         DecimalFormat decimalFormat = new DecimalFormat("£#,###.00");
-        // TODO: find how much in total each company pays to their employees, order result by amount
-        // Barclays plc - £12,184,531.00
-        throw new UnsupportedOperationException();
+        return EMPLOYEES.stream()
+                .collect(Collectors.groupingBy(emp -> emp.getCompany().getName(),
+                        Collectors.reducing(BigDecimal.ZERO, Employee::getSalary, BigDecimal::add)))
+                .entrySet()
+                .stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .map(e -> e.getKey() + ": " + decimalFormat.format(e.getValue()))
+                .collect(Collectors.toList());
     }
 }
