492265f0901b7369114c483c59feea73ac518471
Ha N Tran <hanhattran@kms-technology.com>
2021-03-15 19:23:54 +0700

    Completed repository

--- src/test/java/coaching/repository/RepositoryTestUtils.java
+++ src/test/java/coaching/repository/RepositoryTestUtils.java
@@ -2,13 +2,23 @@ package coaching.repository;
 
 import coaching.model.Staff;
 
+import java.time.LocalDateTime;
+
 public final class RepositoryTestUtils {
 
     private RepositoryTestUtils() {
         // Empty constructor
     }
 
-    public static Staff createMockStaff() {
-        return new Staff();
+    public static Staff createMockStaff(int id, String firstName, String middleName, String lastName, LocalDateTime dob, String phone, String address) {
+        Staff staff = new Staff();
+        staff.setId(id);
+        staff.setFirstName(firstName);
+        staff.setMiddleName(middleName);
+        staff.setLastName(lastName);
+        staff.setDob(dob);
+        staff.setPhone(phone);
+        staff.setAddress(address);
+        return staff;
     }
 }
